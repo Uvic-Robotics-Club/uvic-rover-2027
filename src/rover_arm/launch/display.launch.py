9ff6345e6157b9d8ccf00b3f1ebc84b3d250859d
Simran Cheema <tej11.sc@gmail.com>
2026-09-11 07:20:46 +0000

    added changes to devcontainer/dockerfile to ensure vcxsrv runs correctly; began slowly correcting rover arm urdf for rviz simulation

---
+++ src/rover_arm/launch/display.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory('rover_arm')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'ASY, ROBOTIC ARM, 2026, URDF.urdf')
+    rviz_config = os.path.join(pkg_path, 'urdf.rviz')
+
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz',
+            arguments=['-d', rviz_config],
+        ),
+
+    ])
